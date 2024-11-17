118f57880ffe588d1d3d93cb1ac6bc671faea83a
Aidan <i@aidxn.fun>
2024-11-17 12:41:28 -0500

    style/func: improve design, functionality, accessibility

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -2,7 +2,7 @@ import { Header } from "./header"
 export function HeaderComponent(){
     return (
         <Header>
-            <a>Giv's raffle</a>
+            <a>Giv's Raffle</a>
         </Header>
     )
 }
